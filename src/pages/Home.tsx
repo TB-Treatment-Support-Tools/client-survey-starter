ff2c1bbb7aa75b9d2f6efd373b95589939758d37
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-10-05 14:56:21 -0400

    Add auth headers from keycloak

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { useCallback, useEffect, useState } from 'react';
+import { useKeycloak } from '@react-keycloak/web';
 
-export default function Home(){
-    return <p>Home</p>
+export default function Home() {
+    const { keycloak } = useKeycloak()
+
+
+    const logout = useCallback(() => {
+        keycloak?.logout()
+    }, [keycloak])
+
+    useEffect(() => {
+        if (keycloak) {
+            const requestHeaders: HeadersInit = new Headers();
+            requestHeaders.set('Authorization', `Bearer ${keycloak.token}`)
+            fetch('http://localhost:8100/patients',{headers:requestHeaders}).then(res => { return res.json }).then(json => {
+                console.log(json)
+            })
+        }
+    }, [keycloak])
+
+    return (<div>
+        {keycloak?.authenticated && <button onClick={logout}>Logout</button>}
+        {keycloak?.authenticated && console.log(keycloak.tokenParsed)}
+        <p>Home</p>
+    </div>)
 }
