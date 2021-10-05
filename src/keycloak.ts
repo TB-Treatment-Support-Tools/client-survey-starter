e527664154336e5110b7a4c1201e9ba946413f70
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-10-05 12:30:53 -0400

    Basic keycloak login working

---
+++ src/keycloak.ts
@@ -0,0 +1,11 @@
+import Keycloak from 'keycloak-js';
+
+// Setup Keycloak instance as needed
+// Pass initialization options as required or leave blank to load from 'keycloak.json'
+const keycloak = Keycloak({
+  url: 'https://keycloak.local.app:4400/auth',
+  realm: 'survey-app',
+  clientId: 'react-ts-app'
+});
+
+export default keycloak;
