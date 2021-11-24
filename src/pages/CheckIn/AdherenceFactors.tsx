553eb221baa496e85c26016dd3878c564e099cb3
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-11-24 14:37:41 -0500

    Add full list of reasons for non-adherence

--- src/pages/CheckIn/AdherenceFactors.tsx
+++ src/pages/CheckIn/AdherenceFactors.tsx
@@ -6,15 +6,16 @@ import { Box } from '@mui/system';
 import QuestionText from '../../components/Text/QuestionText';
 import NextButton from './NextButton';
 
+//@TODO - make this conditional based on the answer to the previous question
+//@TODO - dont hardcode these here, move to FHIR Survey definition on HAPI Server 
+const reasons = ["I forgot to take it","I ran out of medication","My medication is lost or stolen","I didn’t have time to take it","I didn’t have it with me today","I didn’t have privacy to take it","I wasn’t feeling well","Other"]
+
 export default function AdherenceFactors() {
     return (<Box padding="1em">
         <QuestionText>What has been causing you to miss doses of your medication?</QuestionText>
-        <p>TODO: This screen will be conditional based on the previous question</p>
         <Box height="1em" />
         <FormGroup>
-            <FormControlLabel control={<Checkbox />} label="Reason One" />
-            <FormControlLabel control={<Checkbox />} label="Reason Two" />
-            <FormControlLabel control={<Checkbox />} label="Other:" />
+            {reasons.map( (each,index) => <FormControlLabel  control={<Checkbox />} label={each} value={index} key={`adherence-reason-${index}`} />)}
             <NextButton />
         </FormGroup>
     </Box>)
