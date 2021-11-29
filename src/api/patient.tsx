f1976aaf05be01a69c81548a908a841cbbd6fb36
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-11-29 12:28:58 -0500

    Fetch the survey on patient home render

--- src/api/patient.tsx
+++ src/api/patient.tsx
@@ -1,5 +1,5 @@
 import { fhirFetch } from "./base";
-import { CarePlan, CodeableConcept, Condition } from "fhir/r4";
+import { CarePlan, CodeableConcept, Condition, Questionnaire } from "fhir/r4";
 
 const riskForHIVCodeableConcept: CodeableConcept = {
     coding: [{
@@ -17,23 +17,6 @@ const positiveHIVCodeableConcept: CodeableConcept = {
     }],
 }
 
-
-// const conditionID = 31;
-
-//Condition will need to be created first
-// const prepCarePlan: CarePlan = {
-//     resourceType: "CarePlan",
-//     status: "active",
-//     intent: "plan",
-//     subject: {
-//         reference: "Patient/1"
-//     },
-//     title: "Monitor PrEP Adherence",
-//     addresses: [{ reference: `Condition/${conditionID}` }]
-
-// }
-
-
 const addCondition = (patientID: string, hivPositive : boolean) => {
     const condition: Condition = {
         resourceType: "Condition",
@@ -46,4 +29,15 @@ const addCondition = (patientID: string, hivPositive : boolean) => {
     fhirFetch(`Condition`, { method: "POST", body: JSON.stringify(condition) })
 }
 
-export {addCondition}
+const getQuestionnaire = async () => {
+    return fhirFetch('Questionnaire/52').then(json => { 
+        if(json.resourceType === "Questionnaire"){
+             return json as Questionnaire 
+        }else{
+            return null;
+        }
+       
+    })
+}
+
+export {addCondition, getQuestionnaire}
--- src/pages/Patient/Home/index.tsx
+++ src/pages/Patient/Home/index.tsx
@@ -7,12 +7,28 @@ import { Box } from '@mui/system';
 import { Link, useLocation } from 'react-router-dom';
 import LearningPreview from '../../../components/LearningPreview';
 import CheckIn from '../../CheckIn';
+import { useEffect, useState } from 'react';
+import { getQuestionnaire } from '../../../api/patient';
+import { Questionnaire } from 'fhir/r4';
+
 
 
 export default function PatientHome() {
 
+
+    const [survey,setSurvey] = useState<Questionnaire | null>(null);
     const location = useLocation();
     const isSurvey = location.pathname.split("/")[1] === "survey"
+
+
+    const fetchQ = async () => {
+        const questionaire = await getQuestionnaire();
+        setSurvey(questionaire);
+    }
+
+    useEffect(()=>{
+        fetchQ();
+    },[])
 
     return (
         <>
@@ -24,6 +40,9 @@ export default function PatientHome() {
                     <AddCircle />
                     <span>Check-In</span>
                 </SectionTitle>
+                <div>
+                    <p>Questionire: {survey ? "Yes" : "No"}</p>
+                    </div>
                 <div className={classes.lastCheckIn}>
                     <span className={classes.lastReport}>You last reported 6 days ago. How have you been since then?</span>
                 </div>
